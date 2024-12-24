14009fa890010052729fa37e3e10d03a24990a9c
linusbayere <linusbayere@gmail.com>
2024-12-24 05:37:55 -0500

    fix authetication bug (add password compare)

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -154,6 +154,11 @@ func (app *application) createTokenHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	if err := user.Password.Compare(payload.Password); err != nil {
+		app.unauthorizedErrorResponse(w, r, err)
+		return
+	}
+
 	// generate token -> add claims
 	claims := jwt.MapClaims{
 		"sub": user.ID,
